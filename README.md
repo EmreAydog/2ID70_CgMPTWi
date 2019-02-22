ce8a2efb3042f25774d7f8eaa52c97117959a674
Smet <o.m.smet@student.tue.nl>
2019-02-22 11:28:18 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 2ID70_CgMPTWi
+
+This is the git repository of group 16 for completing milestone 1. Have a look at the files.
---
+++ q2Create.sql
@@ -0,0 +1,11 @@
+-- this file should contain the code required to create the auxiliary structures, for query 2
+-- this file will be given 10 minutes to run. Then it will be killed
+-- The file will be invoked with timeout 10m psql -d uni -f q2Create.sql
+-- Remember that the database (including the auxiliary structures) needs to be less than 11 GB.
+-- This file will be executed with postgres -d uni -f q2Create.sql 
+-- Example:
+ALTER TABLE Degrees add primary key (DegreeId);
+ALTER TABLE Students add primary key (StudentId);
+ALTER TABLE Teachers add primary key (TeacherId);
+ALTER TABLE Courses add primary key (CourseId);
+ALTER TABLE CourseOffers add primary key (CourseOfferId);
